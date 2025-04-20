10da5cb930afe21ccf8617baa5de5bde4e41af70
Aditya <aditya@akeo.no>
2025-04-20 21:22:18 +0530

    first commit with some patterns

---
+++ 0001_patterns.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern3(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << j+1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern4(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << i+1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern5(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j<n-i; j++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern6(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i; j++)
+        {
+            cout << j+1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout<<" ";
+        }
+        // star
+        for (int j = 0; j < 2*i-1; j++)
+        {
+            cout<<"*";
+        }
+        // space
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    int n;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+        // pattern1(n);
+        // pattern2(n);
+        // pattern3(n);
+        // pattern4(n);
+        // pattern5(n);
+        // pattern6(n);
+        pattern7(n);
+    }
+}
